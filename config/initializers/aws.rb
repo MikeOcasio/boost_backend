3a950903447c38dc9217a2e6e8e8984137b5e78c
Michael Ocasio <mocasio@sstlift.com>
2024-09-23 18:53:08 -0400

    Update: AWS Config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,12 +1,12 @@
 require 'aws-sdk-s3'
 
 Aws.config.update({
-  region: Rails.application.credentials.dig(:amazon, :region),
+  region: Rails.application.credentials.aws[:s3][:region],
   credentials: Aws::Credentials.new(
-    Rails.application.credentials.dig(:aws, :access_key_id),
-    Rails.application.credentials.dig(:aws, :secret_access_key)
+    Rails.application.credentials.aws[:access_key_id],
+    Rails.application.credentials.aws[:secret_access_key]
   )
 })
 
 # Define a constant for the S3 bucket
-S3_BUCKET = Aws::S3::Resource.new.bucket(Rails.application.credentials.dig(:amazon, :bucket))
+S3_BUCKET = Aws::S3::Resource.new.bucket(Rails.application.credentials.aws[:s3][:bucket])
