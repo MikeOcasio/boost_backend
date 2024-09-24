03a09336ed86edc44c9bcf109fed6ee8bd496217
Michael Ocasio <mocasio@sstlift.com>
2024-09-24 17:04:43 -0400

    CSRF Udate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   include Devise::Controllers::Helpers
+
   def health_check
     render plain: "OK", status: :ok
   end
+
+  def csrf_token
+    render json: { csrf_token: form_authenticity_token }
+  end
+
 end
