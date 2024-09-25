ee05f757aef8268a7bf0fbb7737a1ecee9b000e0
Michael Ocasio <mocasio@sstlift.com>
2024-09-25 15:16:03 -0400

    Update: CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       expose: ['X-CSRF-Token'],  # Expose the CSRF token in the response headers
-      credentials: true  # Allow cookies and credentials (for sessions or auth)
+      credentials: false  # Allow cookies and credentials (for sessions or auth)
   end
 end
