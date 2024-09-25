da2a304ca607e8d238f0d930de67c42d575204ec
Michael Ocasio <mocasio@sstlift.com>
2024-09-25 15:14:53 -0400

    Update: CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3006'
+    origins '*'  # Add trusted origins (Next.js frontend and Rails server)
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['X-CSRF-Token'],
-      credentials: true
+      expose: ['X-CSRF-Token'],  # Expose the CSRF token in the response headers
+      credentials: true  # Allow cookies and credentials (for sessions or auth)
   end
 end
