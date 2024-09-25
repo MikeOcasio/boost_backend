70608e6b6d2e1162062765531b1d703ef93ed3c8
Michael Ocasio <mocasio@sstlift.com>
2024-09-25 16:08:41 -0400

    Update Session initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :active_record_store, key: '_operation_boost_session'
+Rails.application.config.session_store :active_record_store, key: '_operation_boost_session', secure: Rails.env.production?
