b8655ca5d067c11768a14ef464ac6a6e6f9d50e4
Michael Ocasio <mocasio@sstlift.com>
2024-10-08 16:34:53 -0400

    Registration: Rollback

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,11 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
 
-    # Debugging line to inspect parameters
-    puts "Params: #{params.inspect}"
-
-    # Check if an image is present and upload it to S3
-    upload_image_to_s3(resource, params[:image]) if params[:image].present?
+    puts "Params: #{params.inspect}"  # Debugging line
 
     resource.save
     yield resource if block_given?
@@ -31,43 +27,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def sign_up_params
-    params.require(:user).permit(
-      :email,
-      :password,
-      :password_confirmation,
-      :role,
-      :first_name,
-      :last_name,
-      :image
-      )
+    params.require(:user).permit(:email, :password, :password_confirmation, :role, :first_name, :last_name)
   end
 
-  def upload_image_to_s3(user, image_param)
-    if image_param.is_a?(ActionDispatch::Http::UploadedFile)
-      # Handle file upload
-      obj = S3_BUCKET.object("users/#{image_param.original_filename}")
-      obj.upload_file(image_param.tempfile)
-      user.image_url = obj.public_url
-    elsif image_param.is_a?(String) && image_param.start_with?('data:image/')
-      # Handle base64 image upload
-      base64_data = image_param.split(',')[1]
-      decoded_data = Base64.decode64(base64_data)
-
-      # Generate a unique filename for the image
-      filename = "users/#{SecureRandom.uuid}.webp"
-
-      Tempfile.create(['user_image', '.webp']) do |temp_file|
-        temp_file.binmode
-        temp_file.write(decoded_data)
-        temp_file.rewind
-
-        # Upload to S3
-        obj = S3_BUCKET.object(filename)
-        obj.upload_file(temp_file)
-        user.image_url = obj.public_url
-      end
-    else
-      raise ArgumentError, "Expected an instance of ActionDispatch::Http::UploadedFile or a base64 string, got #{image_param.class.name}"
-    end
-  end
 end
