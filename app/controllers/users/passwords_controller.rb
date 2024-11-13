7b94a22cd28dce04c4dcd03d3fecce0c84222d08
Michael Ocasio <mocasio@sstlift.com>
2024-11-13 15:31:28 -0500

    Update Routing: Emailing

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,5 +2,12 @@
 module Users
   class PasswordsController < Devise::PasswordsController
     # No custom behavior; this inherits from Devise::PasswordsController
+
+    protected
+
+    # Redirect to the frontend URL after password change
+    def after_resetting_password_path_for(_)
+      Rails.env.development? ? 'http://127.0.0.1:3001/login' : 'https://www.ravenboost.com/login'
+    end
   end
 end
