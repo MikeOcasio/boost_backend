b0d82469c6a8b569ac1871eb85d1d7284c1d92af
Michael Ocasio <mocasio@sstlift.com>
2024-11-13 15:45:35 -0500

    Update Email

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -3,6 +3,15 @@ module Users
   class PasswordsController < Devise::PasswordsController
     # No custom behavior; this inherits from Devise::PasswordsController
 
+    def update
+      super do |resource|
+        if resource.errors.empty?
+          redirect_to after_resetting_password_path_for(resource), allow_other_host: true
+          return # Prevent further execution
+        end
+      end
+    end
+
     protected
 
     # Redirect to the frontend URL after password change
