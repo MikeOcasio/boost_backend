b2bc013ea51011d81a91ab49d7a7ebcd1f6ed80a
Michael Ocasio <michael.a.ocasiofigueroa@gmail.com>
2025-02-20 01:36:30 -0500

    Update: Chat Model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,10 +1,10 @@
 class Chat < ApplicationRecord
   belongs_to :customer, class_name: 'User', optional: true
-  belongs_to :booster, class_name: 'User'
+  belongs_to :skillmaster, class_name: 'User'
   has_many :messages, dependent: :destroy
 
-  validates :booster_id, presence: true
-  validates :customer_id, uniqueness: { scope: :booster_id }, unless: :broadcast?
+  validates :skill_master_id, presence: true
+  validates :customer_id, uniqueness: { scope: :skill_master_id }, unless: :broadcast?
 
   scope :broadcasts, -> { where(broadcast: true) }
 
@@ -12,16 +12,16 @@ class Chat < ApplicationRecord
     transaction do
       chat = create!(
         customer_id: sender.id,
-        booster_id: sender.id,
+        skill_master_id: sender.id,
         broadcast: true,
         title: title
       )
 
       # Create individual chats for each skill master
-      User.where(role: 'skill_master').each do |skill_master|
+      User.where(role: 'skillmaster').each do |skill_master|
         create!(
           customer_id: sender.id,
-          booster_id: skill_master.id,
+          skill_master_id: skill_master.id,
           broadcast: true,
           title: title
         )
